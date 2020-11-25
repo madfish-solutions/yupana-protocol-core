54a95a3e39be3c7736c7d3d3dc683843e1921e74
DzenkivV <viktor.dzenkiv@nure.ua>
2020-11-25 15:18:04 +0200

    fix truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const { accounts } = require('./scripts/sandbox/accounts');
+const { accountsMap } = require('./scripts/sandbox/accounts');
+
 module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
@@ -6,7 +9,7 @@ module.exports = {
       host: "http://localhost",
       port: 8732,
       network_id: "*",
-      secretKey: 'edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq',
+      secretKey: accountsMap.get(accounts[0]),
       type: "tezos"
     }
   }
