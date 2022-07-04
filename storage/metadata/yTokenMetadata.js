7802177f73a61bf74a8eec9c23866be2e59d2a12
Illia Likhoshva <ilyaliko64@gmail.com>
2022-07-04 16:08:29 +0300

    Update metadata

--- storage/metadata/yTokenMetadata.js
+++ storage/metadata/yTokenMetadata.js
@@ -6,13 +6,13 @@ const metadata = MichelsonMap.fromLiteral({
   yupana: Buffer.from(
     JSON.stringify({
       name: "Yupana",
-      version: "v0.3.3",
+      version: "v0.3.4",
       description: "Yupana protocol.",
       authors: ["Madfish.Solutions <https://www.madfish.solutions>"],
       source: {
         tools: ["Ligo", "Flextesa"],
         location:
-          "https://github.com/madfish-solutions/yupana-protocol-core/blob/master/contracts/main/yToken.ligo",
+          "https://github.com/madfish-solutions/yupana-protocol-core/blob/v0.3.4/contracts/main/yToken.ligo",
       },
       homepage: "https://yupana.com",
       interfaces: ["TZIP-012 git 1728fcfe", "TZIP-016"],
