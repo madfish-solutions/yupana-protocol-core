e5d439a7139c1902918413d38940fe2f3901f7b4
Illia Likhoshva <ilyaliko64@gmail.com>
2022-07-06 17:59:07 +0300

    update metadata

--- storage/metadata/yTokenMetadata.js
+++ storage/metadata/yTokenMetadata.js
@@ -5,16 +5,16 @@ const metadata = MichelsonMap.fromLiteral({
   "": Buffer.from("tezos-storage:yupana", "ascii").toString("hex"),
   yupana: Buffer.from(
     JSON.stringify({
-      name: "Yupana",
+      name: "Yupana Finance Lending Protocol",
       version: "v0.3.4",
-      description: "Yupana protocol.",
+      description: "Yupana.Finance is an open-source, decentralized, and non-custodial lending protocol on Tezos built to securely lend and borrow digital assets via smart contracts.",
       authors: ["Madfish.Solutions <https://www.madfish.solutions>"],
       source: {
         tools: ["Ligo", "Flextesa"],
         location:
           "https://github.com/madfish-solutions/yupana-protocol-core/blob/v0.3.4/contracts/main/yToken.ligo",
       },
-      homepage: "https://yupana.com",
+      homepage: "https://yupana.finance",
       interfaces: ["TZIP-012 git 1728fcfe", "TZIP-016"],
       errors: yTokenErrors,
       views: [],
